51b91f769224a9f11bf0e17ac50fda2bc8c3d897
yaskyj <yaskyj@yahoo.com>
2016-04-16 12:20:09 -0500

    Complete udacity SVM project

--- ud120-projects-master/svm/svm_author_id.py
+++ ud120-projects-master/svm/svm_author_id.py
@@ -24,6 +24,22 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel='rbf', C=10000)
+
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+t0 = time()
+clf.fit(features_train, labels_train)
+print 'training time:',round(time()-t0,3),"s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print 'prediction time:',round(time()-t0,3),"s"
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
+print()
 
 #########################################################
 
