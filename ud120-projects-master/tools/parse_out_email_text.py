dc889b13075128d61a2401c920f2d32b2d5f8715
yaskyj <yaskyj@yahoo.com>
2016-04-24 21:26:36 -0500

    Complete Udacity text learning project

--- ud120-projects-master/tools/parse_out_email_text.py
+++ ud120-projects-master/tools/parse_out_email_text.py
@@ -26,13 +26,18 @@ def parseOutText(f):
     if len(content) > 1:
         ### remove punctuation
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
-
+        #print text_string
         ### project part 2: comment out the line below
-        words = text_string
+        #words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
+        stemmer = SnowballStemmer("english")
+        temp_words = text_string.split()
+        temp_words = [x.replace(" ", "") for x in temp_words]
+        temp_words = [stemmer.stem(x) for x in temp_words]
+        words = ' '.join(temp_words)
         
 
 
